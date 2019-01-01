71d098623e6c0da90bac195600a57a3cda00b640
Ashraf Hussain <ashrafhussain.2317.csedu@gmail.com>
2019-01-01 22:32:23 +0600

    Initial commit

---
+++ app/src/main/java/edu/unidhaka/cse/cse2216/keephealthy/MainActivity.kt
@@ -0,0 +1,12 @@
+package edu.unidhaka.cse.cse2216.keephealthy
+
+import android.app.Activity
+import android.os.Bundle
+
+class MainActivity : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
