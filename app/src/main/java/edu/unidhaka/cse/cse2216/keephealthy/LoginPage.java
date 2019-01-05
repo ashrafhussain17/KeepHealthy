4fc4e25c142af0faeb0c582f0162c97356d6ba81
Ashraf Hussain <ashrafhussain.2317.csedu@gmail.com>
2019-01-05 22:22:49 +0600

    first

---
+++ app/src/main/java/edu/unidhaka/cse/cse2216/keephealthy/LoginPage.java
@@ -0,0 +1,62 @@
+package edu.unidhaka.cse.cse2216.keephealthy;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class LoginPage extends Activity {
+
+    Button OKButton;
+    EditText emailBox;
+    EditText passwordBox;
+    Button RegiButton;
+
+    private FirebaseAuth RegistrationAuthe;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        emailBox = findViewById(R.id.email_field);
+        passwordBox = findViewById(R.id.passwordField);
+        OKButton = findViewById(R.id.okButton);
+        RegiButton = findViewById(R.id.Regi);
+
+        RegistrationAuthe = FirebaseAuth.getInstance();
+
+        RegiButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LoginPage.this,Registration.class));
+            }
+        });
+
+        OKButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                login();
+            }
+        });
+    }
+    public void login(){
+        RegistrationAuthe.signInWithEmailAndPassword(emailBox.getText().toString().trim(),passwordBox.getText().toString().trim())
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            Toast.makeText(LoginPage.this,"Successful",Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
+    }
+}
---
+++ app/src/main/java/edu/unidhaka/cse/cse2216/keephealthy/Registration.java
@@ -0,0 +1,59 @@
+package edu.unidhaka.cse.cse2216.keephealthy;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class Registration extends Activity {
+
+
+    Button ROKButton;
+    EditText RemailBox;
+    EditText RpasswordBox;
+
+    private FirebaseAuth registrationAuth;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.registration_layout);
+
+
+        RemailBox = findViewById(R.id.Remail_field);
+        RpasswordBox = findViewById(R.id.RpasswordField);
+        ROKButton = findViewById(R.id.RokButton);
+
+        registrationAuth = FirebaseAuth.getInstance();
+
+        ROKButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                createUser();
+            }
+        });
+
+    }
+
+    public void createUser(){
+        registrationAuth.createUserWithEmailAndPassword(RemailBox.getText().toString().trim(),RpasswordBox.getText().toString().trim())
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            Toast.makeText(Registration.this,"User Created",Toast.LENGTH_LONG).show();
+                            finish();
+                        }
+                    }
+                });
+    }
+}
