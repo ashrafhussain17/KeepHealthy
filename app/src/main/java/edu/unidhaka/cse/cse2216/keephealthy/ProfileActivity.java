9fd1a0f8b20b6c1b7f6140308957e45aad2175d0
Ashraf Hussain <ashrafhussain.2317.csedu@gmail.com>
2019-01-20 22:31:50 +0600

    capture image feature

--- app/src/main/java/edu/unidhaka/cse/cse2216/keephealthy/ProfileActivity.java
+++ app/src/main/java/edu/unidhaka/cse/cse2216/keephealthy/ProfileActivity.java
@@ -1,9 +1,11 @@
 package edu.unidhaka.cse.cse2216.keephealthy;
 
+import android.Manifest;
 import android.annotation.TargetApi;
 import android.app.ActionBar;
 import android.app.Activity;
 import android.content.ComponentName;
+import android.content.ContentValues;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
@@ -16,10 +18,12 @@ import android.os.Build;
 import android.os.Bundle;
 import android.os.Parcelable;
 import android.provider.MediaStore;
+import android.support.annotation.NonNull;
 import android.support.annotation.RequiresApi;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.widget.Toolbar;
+import android.text.TextUtils;
 import android.transition.ChangeBounds;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -43,10 +47,21 @@ public class ProfileActivity extends Activity implements View.OnClickListener {
 
     //view objects
     private TextView textViewUserEmail;
+    private TextView nickname;
+    private TextView height;
+    private TextView weight;
     private Button buttonLogout;
 
     private Button buttonDone;
     private Button birthDate;
+
+    private static final int PERMISSION_CODE = 1000;
+    private static final int IMAGE_CAPTURE_CODE = 1001;
+
+    ImageButton mCaptureBtn;
+    ImageView mImageView;
+
+    Uri image_uri;
 
 
 
@@ -72,25 +87,72 @@ public class ProfileActivity extends Activity implements View.OnClickListener {
 
         //initializing views
         textViewUserEmail = (TextView) findViewById(R.id.textViewUserEmail);
+        nickname = (TextView) findViewById(R.id.nickname);
+        height = (TextView) findViewById(R.id.heightvalue);
+        weight = (TextView) findViewById(R.id.weightvalue);
+
         buttonLogout = (Button) findViewById(R.id.buttonLogout);
 
         buttonDone = (Button) findViewById(R.id.buttonDone);
         birthDate = (Button) findViewById(R.id.birthdate);
 
+
+        mImageView = findViewById(R.id.image_view);
+        mCaptureBtn = findViewById(R.id.capture_image_btn);
+
+        //button click
+        mCaptureBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //if system os is >= marshmallow, request runtime permission
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+                    if (checkSelfPermission(Manifest.permission.CAMERA) ==
+                            PackageManager.PERMISSION_DENIED ||
+                            checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) ==
+                                    PackageManager.PERMISSION_DENIED){
+                        //permission not enabled, request it
+                        String[] permission = {Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE};
+                        //show popup to request permissions
+                        requestPermissions(permission, PERMISSION_CODE);
+                    }
+                    else {
+                        //permission already granted
+                        openCamera();
+                    }
+                }
+                else {
+                    //system os < marshmallow
+                    openCamera();
+                }
+            }
+        });
+
+
+
+        String str1 = height.getText().toString();
+        String str2 = weight.getText().toString();
+        String str3 = nickname.getText().toString();
+
+        if(TextUtils.isEmpty(str1)){
+            height.setError("Please enter your weight");
+            height.requestFocus();
+        }
+
+        if(TextUtils.isEmpty(str2)){
+            weight.setError("Please enter your height");
+            weight.requestFocus();
+        }
+
+        if(TextUtils.isEmpty(str3)){
+            nickname.setError("Please enter your Nickname");
+            nickname.requestFocus();
+        }
+
         //displaying logged in user name
         textViewUserEmail.setText("Welcome "+user.getEmail());
         //test
 
-        //adding listener to button
 
-
-        /*buttonLogout.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                BackLogin();
-            }
-        });
-        */
         buttonLogout.setOnClickListener(this);
 
 
@@ -129,12 +191,6 @@ public class ProfileActivity extends Activity implements View.OnClickListener {
         }
     }
 
- /*   private void BackLogin() {
-        Intent intent = new Intent(this,LoginPage.class);
-        startActivity(intent);
-    }
-    */
-
     private void HomePage() {
         Intent intent = new Intent(this,home_page.class);
         startActivity(intent);
@@ -143,6 +199,48 @@ public class ProfileActivity extends Activity implements View.OnClickListener {
     private void datePicker(){
         Intent intent = new Intent(this,date_picker.class);
         startActivity(intent);
+    }
+
+
+    private void openCamera() {
+        ContentValues values = new ContentValues();
+        values.put(MediaStore.Images.Media.TITLE, "New Picture");
+        values.put(MediaStore.Images.Media.DESCRIPTION, "From the Camera");
+        image_uri = getContentResolver().insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values);
+        //Camera intent
+        Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, image_uri);
+        startActivityForResult(cameraIntent, IMAGE_CAPTURE_CODE);
+    }
+
+    //handling permission result
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        //this method is called, when user presses Allow or Deny from Permission Request Popup
+        switch (requestCode){
+            case PERMISSION_CODE:{
+                if (grantResults.length > 0 && grantResults[0] ==
+                        PackageManager.PERMISSION_GRANTED){
+                    //permission from popup was granted
+                    openCamera();
+                }
+                else {
+                    //permission from popup was denied
+                    Toast.makeText(this, "Permission denied...", Toast.LENGTH_SHORT).show();
+                }
+            }
+        }
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        //called when image was captured from camera
+
+        if (resultCode == RESULT_OK){
+            //set the image captured to our ImageView
+            mImageView.setImageURI(image_uri);
+        }
     }
 
 
